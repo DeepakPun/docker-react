c350f2e2ac28bae701bbbf38b78ba1e856eb75b7
Deepak Pun <pun.deepak1986@gmail.com>
2022-07-25 10:39:30 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<h2>Hello from Reaction!!</h2>
-				<p>React is really cool.</p>
 				<p>Beautified it!!</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
