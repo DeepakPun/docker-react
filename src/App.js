f8374d4986df918ebd8f0ede36c5e6e1e3db31ec
Deepak Pun <pun.deepak1986@gmail.com>
2022-07-25 22:52:54 -0400

    Made small change to app text to test it out

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
 	return (
 		<div className="App">
+			<h3>Looks like Docker is difficult to learn but will make lives lot easier once we get the hang of it.</h3>
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<h2>Hello from Reaction!!</h2>
